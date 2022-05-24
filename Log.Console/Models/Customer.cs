1028080b1ec70b4a96b74c341f9d2584c250013e
renan <renan@interprocess.com.br>
2022-05-24 09:26:11 -0300

    feat: add customer model

---
+++ Log.Console/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using Log.Data.Entity;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace Log.Console.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal TotalPurchases { get; set; }
+        public decimal TotalReturns { get; set; }
+    }
+
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext()
+        {
+            DbInterception.Add(new LogEFInterceptor());
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
